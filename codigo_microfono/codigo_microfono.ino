4fae869dc2c248caa0bf4d4fb25a6c81f0f73cce
Camila Vivo <47572585@est.ort.edu.ar>
2023-08-29 08:54:52 -0300

    Update codigo_microfono.ino
    Codigo que te da la onda de sonido que recibe el microfono pero no lo que escucha

--- codigo_microfono/codigo_microfono.ino
+++ codigo_microfono/codigo_microfono.ino
@@ -1,8 +1,10 @@
+//este codigo devuelve los valores numericos que recibe el microfono
 void setup() {
- 
-
+ pinMode(35,INPUT);
+ Serial.begin(115200);
 }
 
 void loop() {
-  
+  int voice = analogRead(35);
+  Serial.println(voice);
 }
