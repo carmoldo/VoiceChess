fb556aea0e5e8971eb69f22b763e7f2767f471e2
Camila Vivo <47572585@est.ort.edu.ar>
2023-10-05 14:13:55 -0300

    feat codigo_motor.ino
    duplique el codigo anterior del motor con otras salidas para mover el segundo motor

--- Hardware/codigo_dos_motores/codigo_dos_motores.ino
+++ Hardware/codigo_dos_motores/codigo_dos_motores.ino
@@ -1,9 +1,55 @@
-void setup() {
-  // put your setup code here, to run once:
+#define dly 10
 
+void setup() {
+ //seteo los pines de output
+ //STEP 1
+ pinMode(4, OUTPUT);
+ //DIR 1
+ pinMode(5, OUTPUT);
+
+ //STEP 2
+ pinMode(27, OUTPUT);
+ //DIR 2
+ pinMode(14, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //gira para la derecha (1)
+ digitalWrite(5, HIGH);
+  //da una vuelta entera usando 200
+  for(int i = 0; i < 201; i++){
+    digitalWrite(4, HIGH);
+    delay(dly);
+    digitalWrite(4, LOW);
+    delay(dly);
+  }
 
+  //gira para la izquierda (1)
+  digitalWrite(5, LOW);
+  for(int i = 0; i < 201; i++){
+    digitalWrite(4, HIGH);
+    delay(dly);
+    digitalWrite(4, LOW);
+    delay(dly);
+  }
+
+
+  //gira para la derecha (2)
+ digitalWrite(14, HIGH);
+  //da una vuelta entera usando 200
+  for(int i = 0; i < 201; i++){
+    digitalWrite(27, HIGH);
+    delay(dly);
+    digitalWrite(27, LOW);
+    delay(dly);
+  }
+
+  //gira para la izquierda (2)
+  digitalWrite(14, LOW);
+  for(int i = 0; i < 201; i++){
+    digitalWrite(27, HIGH);
+    delay(dly);
+    digitalWrite(27, LOW);
+    delay(dly);
+  }
 }
