3789c1929c6b86a5e743d14bce61adc8f85c4c53
Camila Vivo <47572585@est.ort.edu.ar>
2023-10-06 08:44:21 -0300

    feat test_segundo_motor

--- Hardware/test_segundo_motor/test_segundo_motor.ino
+++ Hardware/test_segundo_motor/test_segundo_motor.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#define stepTWO 27
+#define outputTWO 14
+#define pasos 201
+#define dly 10
 
+void setup() {
+  pinMode(stepTWO, OUTPUT);
+ pinMode(outputTWO, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    //gira para la derecha 
+ digitalWrite(outputTWO, HIGH);
+  //da una vuelta entera usando 200
+  for(int i = 0; i < pasos; i++){
+    digitalWrite(stepTWO, HIGH);
+    delay(dly);
+    digitalWrite(stepTWO, LOW);
+    delay(dly);
+  }
+  //gira para la izquierda 
+  digitalWrite(outputTWO, LOW);
+  for(int i = 0; i < pasos; i++){
+    digitalWrite(stepTWO, HIGH);
+    delay(dly);
+    digitalWrite(stepTWO, LOW);
+    delay(dly);
+  }
 }
